7e2735a7993899dbf8a852e3742849b614a78e20
MarkoVoitovych <searchangeos@gmail.com>
2023-05-22 18:51:41 +0300

    second lesson

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+"use strict";
+var age = 50;
+var names = "Max";
+var toggle = true;
+var empty = null;
+var notInitialized = undefined;
+var callback = function (a) {
+    return 100 + a;
+};
+var anything = -20;
+anything = "Text";
+anything = {};
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+var person = ["Max", 21];
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+var page = {
+    load: Load.READY,
+};
+if (page.load === Load.LOADING) {
+    console.log("page loading");
+}
+if (page.load === Load.READY) {
+    console.log("page ready");
+}
+var optional = 23;
+optional = "44";
+var state;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+var customError = function () {
+    throw new Error("Error");
+};
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
